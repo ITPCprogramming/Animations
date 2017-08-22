c5a38fc0400fa05fa2fcca59fe70ca7241761086
renger151 <renger4596@gmail.com>
2017-08-22 22:44:54 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $("#sub").click(function () {
-        $("#Line").animate({
-            width: "200px",
-            opacity: "0,5"
-        }, "slow")
-    })
-});
