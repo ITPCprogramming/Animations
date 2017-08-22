9d8841e2d874b61560acdf0cd4f068fc5dbaaa7a
renger151 <renger3623@gmail.com>
2017-08-22 21:59:28 +0300

    Line

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#sub").click(function () {
+        $("#Line").animate({
+            width: "200px",
+            opacity: "0,5"
+        }, "slow")
+    })
+});
