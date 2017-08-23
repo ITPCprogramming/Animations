8b290d657605311fb02ecdc411c7a392a08d5aa4
renger151 <renger4596@gmail.com>
2017-08-23 10:01:36 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function Line() {
+            $("#Line").animate({
+                width: "500px",
+                opacity: "0.5"
+            }, "slow");
+    var output = $('#Num'),
+        timer = null,
+        start = 0,
+        end = 80;
+
+
+    timer = setInterval(function () {
+
+                output.text(++start + '%');
+                if (start >= end) {
+                    clearInterval(timer);
+                }
+            }, 0.5);
+}
